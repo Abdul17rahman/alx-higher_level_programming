2ce78970a9ea7e6438db1d1012ea3860319ca1e9
Abdul Rahman <abdulnsamba@gmail.com>
2023-06-10 20:05:50 +0300

    reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    """Print int values in a list in reverse
+
+    Args:
+        my_list: a list of integers defaults to empty
+
+    Returns:
+        Nothing
+    """
+    my_list.sort(reverse = True)
+    for num in my_list:
+        print("{:d}".format(num))
