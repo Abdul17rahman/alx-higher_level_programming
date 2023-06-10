6de22b725cef76ae84a9ba672d028e8da2da0942
Abdul Rahman <abdulnsamba@gmail.com>
2023-06-10 20:24:02 +0300

    replace

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """new_in_list replaces an element at a given index
+
+    Args:
+        my_list: a list of values
+        idx: index of the element
+        element: the element to replace
+
+    Returns:
+        A new changed list
+    """
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        new_list = list(my_list)
+        new_list[idx] = element
+        return new_list
