496c16838df057ea904dd5db0a17d4d6d6c6643e
Abdul Rahman <abdulnsamba@gmail.com>
2023-06-12 12:42:22 +0300

    tupl

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,4 +9,16 @@ def add_tuple(tuple_a=(), tuple_b=()):
     Returns:
         New tuple with added values
     """
-    return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+    max_len = max(len_a, len_b)
+    min_len = min(len_a, len_b)
+    tuple_sum = []
+    for i in range(max_len):
+        if i < min_len:
+            tuple_sum.append(tuple_a[i] + tuple_b[i])
+        elif len_a == max_len:
+            tuple_sum.append(tuple_a[i])
+        else:
+            tuple_sum.append(tuple_b[i])
+    return tuple(tuple_sum)
