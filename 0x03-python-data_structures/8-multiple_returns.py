bf4087797ea03e14168322ef259a95cc72eb54eb
Abdul Rahman <abdulnsamba@gmail.com>
2023-06-12 14:25:27 +0300

    mult ret

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """multiple_returns - returns a tuple from a string
+
+    Args:
+        sentence: string
+
+    Returns:
+        length: length of a string
+        char: First character of a string
+    """
+    if sentence is None:
+        return (0, None)
+    else:
+        return (len(sentence), sentence[0])
