05da7e58e34d4d0bba29474128b7197b396d6e7f
Abdul Rahman <abdulnsamba@gmail.com>
2023-06-14 17:57:39 +0300

    keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """ number_keys - number of keys in a dict
+
+        Args:
+            a_dictionary: dict to loop
+
+        Returns:
+            length of keys
+    """
+    return len(a_dictionary.keys())
