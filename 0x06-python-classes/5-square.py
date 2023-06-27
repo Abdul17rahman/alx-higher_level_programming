eabfb53b1cc0c7b62f42e8c7cbe9f07b3fee940a
Abdul Rahman <abdulnsamba@gmail.com>
2023-06-28 00:58:09 +0300

    print

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+"""This is an empty class"""
+
+
+class Square:
+    """This is an empty sqaure class """
+
+    def __init__(self, size=0):
+        """Initializing the size attribute"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    # property decorator/getter for retrieving the size
+
+    @property
+    def size(self):
+        """This retrives our private attribute"""
+        return self.__size
+
+    # Setter, sets the size attribute
+
+    @size.setter
+    def size(self, value):
+        """Perform validation """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of a square"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for a in range(self.__size):
+                print("#", end="")
+            print()
