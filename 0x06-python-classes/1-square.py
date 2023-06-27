e1ef48c6858029f9327f84253cc8b2832a69867d
Abdul Rahman <abdulnsamba@gmail.com>
2023-06-27 23:55:35 +0300

    square-size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""This is an empty class"""
+
+
+class Square:
+    """This is an empty sqaure class """
+
+    def __init__(self, size):
+        """Initializing the size attribute"""
+        self._size = size
