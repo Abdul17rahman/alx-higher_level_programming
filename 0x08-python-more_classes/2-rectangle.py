a70ef8043cf1efa32d62c888ab0078bda2601886
Abdul Rahman <abdulnsamba@gmail.com>
2023-07-03 22:38:12 +0300

    peri

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -47,3 +47,13 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """Calculates the area of a rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Calculates the perimeter of a Rec"""
+        if self.__width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
