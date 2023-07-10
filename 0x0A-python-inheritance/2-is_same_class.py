1ca0e396869531116c867922327207cdaf100d52
Abdul Rahman <abdulnsamba@gmail.com>
2023-07-10 21:21:56 +0300

    instance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,9 +3,6 @@
 """Module to test instance"""
 
 
-
 def is_same_class(obj, a_class):
     """ Check if an obj is an instance"""
-    if isinstance(obj, a_class):
-        return True
-    return False
+    return isinstance(obj, a_class)
