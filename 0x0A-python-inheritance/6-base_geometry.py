6779a5028033c46b07db1217bee2e328077f0793
Abdul Rahman <abdulnsamba@gmail.com>
2023-07-10 23:28:15 +0300

    except

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+    BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """This is an base geometry class """
+    def __init__(self):
+        """ Initializing the geometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
