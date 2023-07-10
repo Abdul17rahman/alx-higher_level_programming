2153804b3f0acb78bed6e301ce59e1d831e9533d
Abdul Rahman <abdulnsamba@gmail.com>
2023-07-11 02:41:34 +0300

    attr

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""
+    Empty MyInt class
+"""
+
+
+class MyInt(int):
+    """This is an empty base MyInt class """
--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,11 +4,11 @@
 """
 
 
-def add_attribute(mc, name, value):
+def add_attribute(ob, name, value):
     """
         Function add_attribute - adds attribute to an object
         Args:
-            mc: The object
+            ob: The object
             name: The attribute to add to.
             value: The value to be added to attribute
 
@@ -18,7 +18,7 @@ def add_attribute(mc, name, value):
         Returns:
             Nothing
     """
-    if type(mc) not in [int, tuple, dict, str, list, float]:
-        mc.name = value
+    if type(ob) not in [int, tuple, dict, str, list, float]:
+        setattr(ob, name, value)
     else:
         raise TypeError("can't add attribute")
