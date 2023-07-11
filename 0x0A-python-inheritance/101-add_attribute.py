7ca48e6a3091da91e4de634b118864fb62ea7795
Abdul Rahman <abdulnsamba@gmail.com>
2023-07-11 03:16:06 +0300

    atr slots

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,16 +5,6 @@
 def add_attribute(ob, name, value):
     """
         Function add_attribute - adds attribute to an object
-        Args:
-            ob: The object
-            name: The attribute to add to.
-            value: The value to be added to attribute
-
-        Raises:
-            TypeError: If attributed cant be added
-
-        Return:
-            Nothing
     """
     if hasattr(ob, "__slots__") and name not in ob.__slots__:
         raise TypeError("can't add attribute")
