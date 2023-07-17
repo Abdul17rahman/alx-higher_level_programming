5ff75e32ac6777531e897c331224541802446595
Abdul Rahman <abdulnsamba@gmail.com>
2023-07-17 23:13:37 +0300

    16 -1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -65,8 +65,8 @@ class Base:
         if list_objs:
             js_st = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
         else:
-            js_st = []
-        with open(filename, 'w') as file:
+            js_st = cls.to_json_string([])
+        with open(filename, 'w', encoding="utf-8") as file:
             file.write(js_st)
 
     @staticmethod
