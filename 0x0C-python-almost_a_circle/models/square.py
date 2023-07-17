b5e8129adb32452c1a8c0538a91c541422366325
Abdul Rahman <abdulnsamba@gmail.com>
2023-07-17 21:29:46 +0300

    sq

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,9 +12,6 @@ class Square(Rectangle):
     """
     def __init__(self, size, x=0, y=0, id=None):
         """ Initializing the Square """
-        super().valid_input("size", size, "dime")
-        super().valid_input("x", x)
-        super().valid_input("y", y)
         super().__init__(size, size, x, y, id)
 
     # Getter and setter methods for size
