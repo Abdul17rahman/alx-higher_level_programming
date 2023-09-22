acdeafe2bcd5a988e510a98cf83398c1fc928228
Abdul Rahman <abdulnsamba@gmail.com>
2023-09-23 00:28:53 +0300

    m

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-from sqlalchemy import create_engine, Column, Integer, String
+"""
+Module for the class definition of a Cities.
+"""
+
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -8,7 +12,11 @@ Base = declarative_base()
 
 
 class City(Base):
+    """
+    Cities model that inherits from Base
+    """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True
                 unique=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+"""
+Module for the class definition of a State.
+"""
+
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -8,6 +12,9 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    State model that inherits from Base
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True
                 unique=True)
