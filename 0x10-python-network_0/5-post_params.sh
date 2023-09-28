758723d8b5eeb978cf088f3dabc95b355a520661
Abdul Rahman <abdulnsamba@gmail.com>
2023-09-28 23:11:41 +0300

    post

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a post request with parameters
-curl --data "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -F "email=test@gmail.com&subject=I will always be here for PLD" -F "$1"
