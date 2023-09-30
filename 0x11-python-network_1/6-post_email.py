7fc8e4df700aaa91a6e9e895a5e39b94bb20dae6
Abdul Rahman <abdulnsamba@gmail.com>
2023-09-30 12:52:28 +0300

    post

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,8 +3,14 @@
 """ This module fetches using urllib """
 
 
+import sys
 import requests
 
-with urllib.request.urlopen(sys.argv[1]) as res:
-    head = res.headers
-    print(head['X-Request-Id'])
+if __name__ == "__main__":
+    # Get the commands line args
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {'email': email}
+    # Retrieve the response
+    page = requests.post(url, data=payload)
+    print(page.text)
