a05fd77cc111a6005aa5c459ea6ffe40a2664ad8
Abdul Rahman <abdulnsamba@gmail.com>
2023-09-30 19:30:27 +0300

    js

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,15 +11,13 @@ if __name__ == "__main__":
     q = sys.argv[1] if len(sys.argv) > 1 else ""
     payload = {'q': q}
     # Retrieve the response
-    res = requests.post('http://0.0.0.0:5000/search_user', data=payload).json()
+    res = requests.post('http://0.0.0.0:5000/search_user', data=payload)
     # check if response is valid json
-    # if res.status_code == 200:
-    # res_json = res.json()
     try:
-        if isinstance(res, dict):
-            if res:
-                print("[{}] {}".format(res['id'], res['name']))
-            else:
-                print('No result')
+        res_json = res.json()
+        if res_json:
+            print("[{}] {}".format(res_json['id'], res_json['name']))
+        else:
+            print('No result')
     except ValueError:
         print('Not a valid JSON')
