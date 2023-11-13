000362b51eb7cd60a9ab5506fcc81a3889804262
Abdul Rahman <abdulnsamba@gmail.com>
2023-11-13 12:10:09 +0300

    semi

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let c = "C is fun"
-let py = "Python is cool"
-let myVar = "JavaScript is amazing"
-console.log(c)
-console.log(py)
-console.log(myVar)
+const c = 'C is fun';
+const py = 'Python is cool';
+const myVar = 'JavaScript is amazing';
+console.log(c);
+console.log(py);
+console.log(myVar);
