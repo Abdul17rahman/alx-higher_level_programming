6bb16fe466456bc423492f018baacfb0ac683f1d
Abdul Rahman <abdulnsamba@gmail.com>
2023-11-13 13:32:00 +0300

    fact

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const x = parseInt(process.argv[2]);
+
+function factorial (a) {
+  let fact = 1;
+  for (let i = 1; i <= a; i++) {
+    fact *= i;
+  }
+  return fact;
+}
+console.log(factorial(x));
