3ddf24df298fc3f5b6e87949c8aec053b8549804
Abdul Rahman <abdulnsamba@gmail.com>
2023-11-18 00:05:22 +0300

    filter2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+  This is a script that lists all states from the database hbtn_0e_0_usa
+    using MySQLdb ORM
+"""
+
+
+def select_states(username, password, database, name):
+    """ Function that print the states """
+    import MySQLdb
+
+    database = MySQLdb.connect(user=username, passwd=password, db=database)
+    cur = database.cursor()
+    cur.execute("SELECT * FROM states WHERE name = \'{}\'".format(name))
+    items = cur.fetchall()
+    for item in items:
+        if item[1] == name:
+            print(item)
+    cur.close()
+    database.close()
+
+
+if __name__ == "__main__":
+    import sys
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+    name = sys.argv[4]
+    select_states(usr, pwd, db, name)
