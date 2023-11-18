310378cb8be4dfe56c592320d13e97e57c8e53a7
Abdul Rahman <abdulnsamba@gmail.com>
2023-11-19 00:19:57 +0300

    del

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+""" Module to query the database for all states"""
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+
+def delete_states_with_a():
+    """ Function that lists all states containing a"""
+    session = Session()
+    states = session.query(State).filter(State.name.like('%a%')).all()
+    for state in states:
+        session.delete(state)
+    session.commit()
+    session.close()
+
+
+if __name__ == "__main__":
+    import sys
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+    host = "localhost"
+    path = f'mysql+mysqldb://{usr}:{pwd}@{host}/{db}'
+    engine = create_engine(path, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    delete_states_with_a()
