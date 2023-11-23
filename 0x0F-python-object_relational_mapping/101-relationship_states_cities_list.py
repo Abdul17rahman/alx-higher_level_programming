0e0321a2d5dee6f93f1e13cf62710313dd9db594
Abdul Rahman <abdulnsamba@gmail.com>
2023-11-23 18:03:56 +0300

    states rel

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+""" Module to query the database for all states"""
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from relationship_state import Base, State
+from relationship_city import City
+
+
+def add_state_city():
+    """ Function that adds both state and city"""
+    session = Session()
+    states = session.query(State).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"    {city.id}: {city.name}")
+    session.close()
+
+
+if __name__ == "__main__":
+    import sys
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+    host = "localhost"
+    path = f'mysql+mysqldb://{usr}:{pwd}@{host}/{db}'
+    engine = create_engine(path, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    add_state_city()
