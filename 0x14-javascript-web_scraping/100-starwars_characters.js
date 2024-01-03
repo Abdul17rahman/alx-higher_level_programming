72fa169e2be14b6a47ffabf617dfd3fbf4a73183
Abdul Rahman <abdulnsamba@gmail.com>
2024-01-04 02:17:55 +0300

    char-fix

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -8,8 +8,9 @@ const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
 request(url, (err, res, body) => {
   if (err) console.log(err);
   const movie = JSON.parse(body);
-  movie.characters.map(char_url => {
-    request(char_url, (err, res, body) => {
+  movie.characters.forEach(charUrl => {
+    request(charUrl, (err, res, body) => {
+      if (err) throw err;
       console.log(JSON.parse(body).name);
     });
   });
