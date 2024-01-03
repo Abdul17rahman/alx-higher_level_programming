27ee220fd6205a1c36dee43af7a8a7c3c38151de
Abdul Rahman <abdulnsamba@gmail.com>
2024-01-04 01:21:41 +0300

    semi s fix

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,11 +5,11 @@ const request = require('request');
 const fs = require('fs');
 
 const url = process.argv[2];
-const path = process.argv[3]
+const path = process.argv[3];
 
 request(url, (err, res, body) => {
   if (err) console.log(err);
   fs.writeFile(path, body, 'utf-8', (er) => {
     if (er) console.log(er);
-  })
+  });
 });
