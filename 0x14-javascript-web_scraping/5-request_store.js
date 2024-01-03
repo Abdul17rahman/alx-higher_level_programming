b1d2c1855bb1f051116ebb6ba76cd6166257878a
Abdul Rahman <abdulnsamba@gmail.com>
2024-01-04 01:20:41 +0300

    loren

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Get Movies where char is Wedge
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const path = process.argv[3]
+
+request(url, (err, res, body) => {
+  if (err) console.log(err);
+  fs.writeFile(path, body, 'utf-8', (er) => {
+    if (er) console.log(er);
+  })
+});
