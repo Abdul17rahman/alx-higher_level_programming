fd19fc17439237c88291f5c5e8e68e34de40bb6e
Abdul Rahman <abdulnsamba@gmail.com>
2024-01-10 23:32:51 +0300

    translate

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+$(document).ready(() => {
+  $('INPUT#btn_translate').click(() => {
+    const langCode = $('INPUT#language_code').val();
+    const url = 'https://stefanbohacek.com/hellosalut/?lang=' + langCode;
+
+    $.get(url, (result) => {
+      $('DIV#hello').html(result.hello);
+    });
+  });
+});
